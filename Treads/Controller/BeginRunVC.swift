b6d3acec3bb21562010f7debf10bbb3109a69cce
Shone <shonesrbija@yahoo.com>
2019-07-17 12:34:38 +0200

    dodavanje prozora za poslednje trcanje

--- Treads/Controller/BeginRunVC.swift
+++ Treads/Controller/BeginRunVC.swift
@@ -12,6 +12,12 @@ import MapKit
 class BeginRunVC: LocationVC {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var lastRunCloseBtn: UIButton!
+    @IBOutlet weak var paceLbl: UILabel!
+    @IBOutlet weak var distanceLbl: UILabel!
+    @IBOutlet weak var durationLbl: UILabel!
+    @IBOutlet weak var lastRunBgView: UIView!
+    @IBOutlet weak var lastRunStack: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,11 +31,36 @@ class BeginRunVC: LocationVC {
         //manager?.delegate = self as? CLLocationManagerDelegate  //ne treba nam ovo jer smo dole spustili ekstenziju i nema potrebe za kastovanje
         manager?.delegate = self
         manager?.startUpdatingLocation()
+        getLastRun()
+    }
+    
+    @IBAction func lastRunCloseBtnPressed(_ sender: Any) {
+        zatvaranjeLastRun(da: true)
+    }
+    func zatvaranjeLastRun(da: Bool){
+        lastRunStack.isHidden = da
+        lastRunBgView.isHidden = da
+        lastRunCloseBtn.isHidden = da
+    }
+    
+    
+    func getLastRun() {
+        guard let lastRun = Run.getAllRuns()?.first else {
+            zatvaranjeLastRun(da: true)
+            return
+            
+        }
+        zatvaranjeLastRun(da: false)
+        paceLbl.text = lastRun.pace.formatTimeDurationToString()
+        distanceLbl.text = "\(lastRun.distance.metersToKilometers(place: 2)) km"
+        durationLbl.text = lastRun.duration.formatTimeDurationToString()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         manager?.stopUpdatingLocation()
     }
+    
+    
     
     @IBAction func locationCenterBtnPressed(_ sender: Any) {
         
